73850e1c38e7f079dfd1e658acd978dbb58c7c11
Mostafa <candlelearner.m.dev@gmail.com>
2025-08-28 10:36:07 +0100

    day 1 part one solved

---
+++ Day 1/Part One/solution.mjs
@@ -0,0 +1,17 @@
+import fs, {readFileSync} from 'fs';
+
+let floor = 0;
+
+const input = readFileSync('./input.txt').toString();
+
+// for (let i = 0; i < input.length; i++) {
+//   if (input[i] === '(') {
+//     floor++;
+//   } else if (input[i] === ')') {
+//     floor--;
+//   }
+// }
+
+floor = [...input].reduce((acc, curr) => curr === '(' ? acc + 1: acc - 1, 0);
+
+console.log(floor);
